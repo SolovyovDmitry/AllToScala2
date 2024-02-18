2ae3ec3502a98ce2949035b9416ef2ef30fb3a5d
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2024-02-18 23:29:59 +0300

    add snippet

--- Handler/Program.cs
+++ Handler/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Handler;
+﻿using Microsoft.Extensions.Logging;
+
+namespace Handler;
 
 class Program
 {
@@ -6,4 +8,60 @@ class Program
   {
     Console.WriteLine("Hello, World!");
   }
+}
+
+interface IHandler 
+{
+  TimeSpan Timeout { get; }
+   
+  Task PerformOperation(CancellationToken cancellationToken);
+}
+
+class Handler: IHandler
+{
+  private readonly IConsumer _consumer;
+  private readonly IPublisher _publisher;
+  private readonly ILogger<Handler> _logger;
+   
+  public TimeSpan Timeout { get; }
+   
+  public Handler(
+    TimeSpan timeout,
+    IConsumer consumer,
+    IPublisher publisher,
+    ILogger<Handler> logger)
+  {
+    Timeout = timeout;
+     
+    _consumer = consumer;
+    _publisher = publisher;   
+    _logger = logger;
+  }
+   
+  public Task PerformOperation(CancellationToken cancellationToken)
+  {
+    //TODO: place code here
+     
+    return Task.CompletedTask;
+  }
+}
+
+record Payload(string Origin, byte[] Data);
+record Address(string DataCenter, string NodeId);
+record Event(IReadOnlyCollection<Address> Recipients, Payload Payload);
+
+enum SendResult
+{
+  Accepted,
+  Rejected
+}
+
+interface IConsumer
+{
+  Task<Event> ReadData();
+}
+
+interface IPublisher
+{
+  Task<SendResult> SendData(Address address, Payload payload);   
 }
